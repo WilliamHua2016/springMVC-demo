bb9409a2a601b6a5b60a56df3a1b44b63d9b5043
huawai <huawai@geerong.com>
2017-01-17 15:33:51 +0800

    add AppPropertites

--- src/main/java/org/william/controller/HelloController.java
+++ src/main/java/org/william/controller/HelloController.java
@@ -1,17 +1,23 @@
 package org.william.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.william.common.AppProperties;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 	
+	@Autowired
+	private AppProperties appProperties;
+	
 	@RequestMapping(value="/sayHello",method=RequestMethod.GET,produces="application/json;charset=UTF-8")
 	@ResponseBody
 	public String sayHello() {
+		System.out.println("testKey:"+appProperties.getTestKey());
 		return "hello william !";
 	}
 
