28f8d125152fbeb2566110729eb50d4262755517
huawai <huawai@geerong.com>
2017-02-03 14:41:39 +0800

    add ThreadPoolTaskExecutor

--- src/main/java/org/william/job/SpringTaskJobByXml.java
+++ src/main/java/org/william/job/SpringTaskJobByXml.java
@@ -3,10 +3,16 @@ package org.william.job;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.stereotype.Service;
+import org.william.util.LoggerUtil;
 
 @Service("springTaskJobByXml") 
 public class SpringTaskJobByXml {
+	
+	@Autowired
+	private ThreadPoolTaskExecutor taskExecutor;
 	
 	public void springTaskJob(){
 		
@@ -15,6 +21,17 @@ public class SpringTaskJobByXml {
         SimpleDateFormat sim = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
         String dateStr = sim.format(date);  
         System.out.println("这是spring task xml配置方式定时器，每五秒执行一次,当前时间：" + dateStr+"END----");  	
+        callThreadPoolTask();
     }
+	
+	private void callThreadPoolTask() {
+		taskExecutor.execute(new Runnable() {
+			
+			@Override
+			public void run() {
+				LoggerUtil.info(this, "ThreadPoolTaskExecutor demo");
+			}
+		});
+	}
 
 }
